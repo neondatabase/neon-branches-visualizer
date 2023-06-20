a7218459d226459d7d44806a2c752d06a3f4e8fd
Mahmoud Abdelwahab <hey@mahmoud.codes>
2023-06-21 02:47:19 +0300

    add crete endpoint to SDK

--- src/utils/neon/resources/Endpoint/index.ts
+++ src/utils/neon/resources/Endpoint/index.ts
@@ -2,6 +2,7 @@ import { components } from '../../types';
 import type { Neon } from '../../sdk';
 
 type EndpointsResponse = components['schemas']['EndpointsResponse'];
+type EndpointCreateRequest = components['schemas']['EndpointCreateRequest'];
 
 export class Endpoint {
   constructor(private readonly neon: Neon) {}
@@ -12,5 +13,19 @@ export class Endpoint {
     );
 
     return data.endpoints;
+  }
+
+  async create(projectId: string, branchId: string) {
+    const { data } = await this.neon.post<EndpointCreateRequest>(
+      `/projects/${projectId}/endpoints`,
+      {
+        endpoint: {
+          type: 'read_write',
+          branch_id: branchId,
+        },
+      }
+    );
+
+    return data.endpoint;
   }
 }
